f2927e8ff475bab38dfffd3d101b176f009af467
matejmeglic <matej@matejmeglic.com>
2020-04-30 20:12:28 +0200

    Api 3

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
+var image_link = ""
+
+class App extends Component {
 
 
-function App() {
+  componentDidMount() {
+    this.loadData()
+    setInterval(this.loadData, 30000);
+  }
 
-  function getImage() {
+  loadData() {
+     try {
+      var request = new XMLHttpRequest();
 
-  var request = new XMLHttpRequest();
+      const res = request.open('GET', 'https://api.github.com/repos/matejmeglic/raspistill-timelapse/contents/public/img/', true);
+      
+      request.onload = function() {
+        var data = JSON.parse(this.response);
+        // console.log(data);
+        // console.log(data[0]);
+        // console.log(data[0].download_url); 
+        image_link = String(data[0].download_url)
+      }
+      
+    
+      request.send()
+    
 
-  request.open('GET', 'https://api.github.com/repos/matejmeglic/raspistill-timelapse/contents/public/img/', true);
-  
-  request.onload = function() {
-    var data = JSON.parse(this.response);
-    console.log(data);
-    console.log(data[0]);
-    console.log(data[0].download_url);
-    var image_link = String(data[0].download_url)
+        this.setState({
+          image_link: image_link,
+           
+        })
+        
+    } catch (e) {
+        console.log(e);
+    }
   }
   
 
-  request.send()
-}
   
- 
+render () {
+ console.log(this.state)
   return (
 
     <div className="App">
       <header className="App-header">
-        <img src={image_link} className="Image" alt="Image" />
+        <img src={this.state} className="timelapse" alt="timelapse" />
       </header>
     </div>
   );
 }
+}
+
 
 export default App;
+
+
